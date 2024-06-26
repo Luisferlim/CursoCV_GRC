1a85eac10e214ff7c3278905f09ebcca4264f236
Luisferlim <luisfernandolim.314@gmail.com>
2024-06-26 20:06:39 -0300

    modulo4aula1

---
+++ essenciais.py
@@ -0,0 +1,33 @@
+import cv2 as cv
+
+img = cv.imread('Fotos/ufc.jpg')
+cv.imshow('base', img)
+
+#coversao
+cinza = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('cinza', cinza)
+
+#diminuir noise (ruido) 
+blur = cv.GaussianBlur(cinza, (3,3), cv.BORDER_DEFAULT)
+cv.imshow('suavizacao', blur)
+
+#edge cascade -> deteccao de borda
+canny = cv.Canny(blur, 125, 175)
+cv.imshow('borda', canny)
+
+#dilating -> dilatar e eroding -> erosao 
+dilatado = cv.dilate(canny, (7,7), iterations=3)
+cv.imshow("dilatacao", dilatado)
+
+#dilating -> dilatar e eroding -> erosao 
+erodir = cv.erode(dilatado, (7,7), iterations=3)
+cv.imshow("erodido", erodir)
+
+#cortar
+corte = img[50:200, 200:400]
+cv.imshow('corte', corte)
+
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()

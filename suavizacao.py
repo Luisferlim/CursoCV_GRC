0bb9ca541f92c82968d16f0cd987826b70eddb86
Luisferlim <luisfernandolim.314@gmail.com>
2024-06-27 20:15:09 -0300

    adicionando uma comparacao

--- suavizacao.py
+++ suavizacao.py
@@ -4,11 +4,11 @@ img = cv.imread('Fotos/ufc.jpg')
 cv.imshow("ufc", img)
 
 #media simples
-media = cv.blur(img,(7,7))
+media = cv.blur(img,(3,3))
 cv.imshow("media", media)
 
 #gaussiana melhor - mais esforco computacional
-gauss = cv.GaussianBlur(img, (7,7), 0)
+gauss = cv.GaussianBlur(img, (3,3), 0)
 cv.imshow("gauss", gauss)
 
 #mediana muito bom em reduzir ruido (plastico)
@@ -20,6 +20,20 @@ bilateral = cv.bilateralFilter(img, 10, 35, 25)
 cv.imshow("bilateral", bilateral)
 
 
+#brincadeirinhas do luis
+
+# mediaCanny = cv.Canny(media, 125, 175)
+# gaussCanny = cv.Canny(gauss, 125, 175)
+# medianaCanny = cv.Canny(mediana, 125, 175)
+# biCanny = cv.Canny(bilateral, 125, 175)
+
+# cv.imshow('av_canny', averageCanny)
+# cv.imshow('ga_canny', gaussCanny)
+# cv.imshow('me_canny', medianCanny)
+# cv.imshow('bi_canny', biCanny)
+
+#fazendo o canny em todas as suavizacoes que a gente fez
+#bitwise fica para a prox aula
 
 cv.waitKey(0)
 cv.destroyAllWindows()
